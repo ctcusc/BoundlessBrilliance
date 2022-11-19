90e042ef3d5de2bb8cba5f2fa60797415070e422
Raymond Kuan <rkuan@usc.edu>
2022-11-19 14:37:04 -0800

    Create Example API for Sprint 1

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,25 +1,26 @@
-const { connect } = require('../db/db.config');
+require("dotenv").config();
+const cors = require("cors");
+const db = require("../db");
+const morgan = require("morgan");
 
 
 class userController {
-
-    db = {};
-
-    constructor() {
-        this.db = connect();
-        // For Development
-        this.db.sequelize.sync({ force: true }).then(() => {
-            console.log("Drop and re-sync db.");
-        });
-    }
 
     async createUser(user) {
         // Sprint 0: Wonjun
     
     }
 
-    async validateUser(user) {
-        // Sprint 0: Evans
+    async validateUser(req) {
+        // Sprint 0: Evans 
+        console.log(req.body.id);
+        const reviews = await db.query(
+            "select assignee from tasks where tasks.id = $1",
+            [req.body.id]
+          );
+
+        console.log(reviews.rows);
+        return reviews.rows
     }
 
     async approveUser(user) {
