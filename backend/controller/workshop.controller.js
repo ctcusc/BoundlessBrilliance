461b16f58daa97056d6cdc29935af242459b08c3
Olivia Lai <olivialai886@gmail.com>
2022-11-30 14:36:04 -0800

    createWorkshop and editWorkshop

--- backend/controller/workshop.controller.js
+++ backend/controller/workshop.controller.js
@@ -8,11 +8,29 @@ class workshopController {
 
     async createWorkshop(workshop) {
         // Sprint 0: Olivia
+        let data = {};
+        try {
+            data = await this.db.workshop.create(workshop);
+        } catch(err) {
+            logger.error('Error::' + err);
+        }
+        return data;
     
     }
 
     async editWorkshop(workshop) {
         // Sprint 0: Olivia
+        let data = {};
+        try {
+            data = await this.db.workshop.update({...workshop}, {
+                where: {
+                    workshop_id: workshop.workshop_id
+                }
+            });
+        } catch(err) {
+            logger.error('Error::' + err);
+        }
+        return data;
     }
 
     async assignUser(user, workshop) {
