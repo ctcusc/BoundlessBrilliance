b02c77ace4a1983111dee6571cc26f7e82d51d87
Raymond Kuan <rkuan@usc.edu>
2022-12-01 21:17:45 -0800

    clean up sprint 0

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -5,18 +5,6 @@ const morgan = require("morgan");
 
 class userController {
 
-  async example(req) {
-    // Example of query
-    console.log(req.body.id);
-    const reviews = await db.query(
-      "select assignee from tasks where tasks.id = $1",
-      [req.body.id]
-    );
-
-    console.log(reviews.rows);
-    return reviews.rows;
-  }
-
   async createUser(user) {
     // Sprint 0: Wonjun
   }
@@ -59,8 +47,6 @@ class userController {
   }
 
   async approveUser(user_id) {
-    // Sprint 0: Fred
-    console.log(user_id);
     const result = await db.query(
       "UPDATE user_status SET user_status = 1 WHERE user_id = $1;",
       [user_id]
@@ -68,7 +54,5 @@ class userController {
     return result.rows[0];
   }
 }
-
         
-module.exports = new userController();
-        
+module.exports = new userController();   
--- backend/controller/workshop.controller.js
+++ backend/controller/workshop.controller.js
@@ -7,8 +7,6 @@ const morgan = require("morgan");
 class workshopController {
 
     async createWorkshop(req) {
-        // Sprint 0: Olivia
-        console.log(req.body.workshop_description);
         const result = await db.query(
             "INSERT INTO workshop (workshop_name, workshop_description, workshop_date, workshop_time, workshop_duration, workshop_chapter, workshop_num_presentors, workshop_is_virtual) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);",
             [req.body.workshop_name, req.body.workshop_description, req.body.workshop_date, req.body.workshop_time, req.body.workshop_duration, req.body.workshop_chapter, req.body.workshop_num_presentors, req.body.workshop_is_virtual]
@@ -17,8 +15,6 @@ class workshopController {
     }
 
     async editWorkshop(req, workshop_id) {
-        // Sprint 0: Olivia
-        console.log(req.body.workshop_description);
         const result = await db.query(
             "UPDATE workshop SET workshop_name = $1, workshop_description = $2, workshop_date = $3, workshop_time = $4, workshop_duration = $5, workshop_chapter = $6, workshop_num_presentors = $7, workshop_is_virtual = $8 WHERE workshop_id = $9;",
             [req.body.workshop_name, req.body.workshop_description, req.body.workshop_date, req.body.workshop_time, req.body.workshop_duration, req.body.workshop_chapter, req.body.workshop_num_presentors, req.body.workshop_is_virtual, workshop_id]
