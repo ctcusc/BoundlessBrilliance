3e135bf801515b9b04787c3aa94b5665231e66cf
Natalie Abreu <nrabreu@usc.edu>
2023-01-22 12:21:07 -0800

    workshop controller

--- backend/controller/workshop.controller.js
+++ backend/controller/workshop.controller.js
@@ -8,7 +8,7 @@ class workshopController {
 
     async createWorkshop(workshop) {
         // Sprint 0: Olivia
-    
+
     }
 
     async editWorkshop(workshop) {
@@ -17,8 +17,16 @@ class workshopController {
 
     async assignUser(user, workshop) {
         // Sprint 0: Natalie
+        try {
+            await db.query(
+                `INSERT INTO "workshop_assignments" ("user_id", "workshop_id", "has_accepted")  
+                 VALUES ($1, $2, $3)`, [user.id, workshop.id, false]); // sends queries
+            return true;
+        } catch (error) {
+            console.error(error.stack);
+            return false;
+        }
     }
-
 
 }
 
