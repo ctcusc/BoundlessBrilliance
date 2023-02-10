31045f45eef8a7cb3975c1dc227cae315f11674b
Natalie Abreu <nrabreu@usc.edu>
2023-02-10 12:46:25 -0800

    Integrates upcoming workshops placeholder

--- client/my-app/src/routes/Home.jsx
+++ client/my-app/src/routes/Home.jsx
@@ -3,26 +3,32 @@ import Header from "../components/Header"
 import HomePageTab from "../components/HomePageTab"
 import { Box, Card, Grid } from '@mui/material';
 import WorkshopCard from '../components/WorkshopCard'
+import WorkshopAssignmentPlaceholder from '../components/WorkshopAssignmentPlaceholder'
+import WorkshopUpcomingPlaceholder from '../components/WorkshopUpcomingPlaceholder'
 import sampleData from '../components/workshopCardSample.json'
 
+//const sampleData = [];
 const Home = () => {
     return (
         <div >
             <Header />
-            <div style={{ backgroundColor: '#FBF8F2', paddingLeft: '5%', paddingBottom: '40px' }}>
+            <div style={{ backgroundColor: '#FBF8F2', paddingLeft: '5%', paddingBottom: '40px', minHeight: '90vh' }}>
                 <HomePageTab />
-                <Box sx={{ flexGrow: 1, width: '90%' }}>
-                    <Grid container spacing={4}>
-                        {sampleData.map((data) => (
-                            <Grid item xs={5}>
-                                <WorkshopCard workshop={data}></WorkshopCard>
-                            </Grid>
-                        ))
-                        }
+                {sampleData.length > 0 ?
+                    <Box sx={{ flexGrow: 1, width: '90%' }}>
+                        <Grid container spacing={4}>
+                            {sampleData.map((data) => (
+                                <Grid item xs={5}>
+                                    <WorkshopCard workshop={data}></WorkshopCard>
+                                </Grid>
+                            ))
+                            }
 
 
-                    </Grid>
-                </Box>
+                        </Grid>
+                    </Box>
+                    : <WorkshopUpcomingPlaceholder />
+                }
             </div>
         </div >
     );
