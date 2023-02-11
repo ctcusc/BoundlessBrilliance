74356e45f4a68efa7461bb0c99742b76c7bb3c2a
Natalie Abreu <nrabreu@usc.edu>
2023-02-10 16:43:22 -0800

    Adds state for r tab switching

--- client/my-app/src/components/Header.jsx
+++ client/my-app/src/components/Header.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import headerLogo from '../images/logo.svg';
 import emailLogo from '../images/email.svg';
 
-import '../index.css'
-
 
 // import {Email} from '@mui/icons-material/Email';
 
@@ -11,7 +9,7 @@ import '../index.css'
 const Header = () => {
     return (
 
-        <div style={{ fontSize: "50px", height: "130px", fontFamily: 'Avenir' }}>
+        <div style={{ fontSize: "50px", height: "130px" }}>
             <div className="border d-flex align-items-center" style={{ position: "relative", height: "100%", width: "full", boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
                 <img src={headerLogo} style={{ position: "relative", left: "6.0%", width: "21.52%", bottom: '6px' }} />
                 <img src={emailLogo} style={{ position: "absolute", right: "6.5%", width: "1.785%" }} />
