ba89c927d4e9811b67d1e4b7c103dbcd583ec054
Natalie Abreu <nrabreu@usc.edu>
2023-02-10 16:43:22 -0800

    Adds state for r tab switching

--- client/my-app/src/routes/Home.jsx
+++ client/my-app/src/routes/Home.jsx
@@ -1,7 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "../components/Header"
 import HomePageTab from "../components/HomePageTab"
 import { Box, Card, Grid } from '@mui/material';
+import AdminWorkshopCard from '../components/AdminWorkshopCard'
+
 import WorkshopCard from '../components/WorkshopCard'
 import WorkshopAssignmentPlaceholder from '../components/WorkshopAssignmentPlaceholder'
 import WorkshopUpcomingPlaceholder from '../components/WorkshopUpcomingPlaceholder'
@@ -9,26 +11,49 @@ import sampleData from '../components/workshopCardSample.json'
 
 //const sampleData = [];
 const Home = () => {
+    const [tab, setTab] = useState(0);
+
     return (
         <div >
             <Header />
             <div style={{ backgroundColor: '#FBF8F2', paddingLeft: '5%', paddingBottom: '40px', minHeight: '90vh' }}>
-                <HomePageTab />
-                {sampleData.length > 0 ?
-                    <Box sx={{ flexGrow: 1, width: '90%' }}>
-                        <Grid container spacing={4}>
-                            {sampleData.map((data) => (
-                                <Grid item xs={5}>
-                                    <WorkshopCard workshop={data}></WorkshopCard>
-                                </Grid>
-                            ))
-                            }
+                <HomePageTab pageIndex={tab} setPageIndex={setTab} />
+
+                {tab == 0 ?
+                    sampleData.length > 0 ?
+                        <Box sx={{ flexGrow: 1, width: '90%' }}>
+                            <Grid container spacing={4}>
+                                {sampleData.map((data) => (
+                                    <Grid item xs={5}>
+                                        <WorkshopCard workshop={data}></WorkshopCard>
+                                    </Grid>
+                                ))
+                                }
 
 
-                        </Grid>
-                    </Box>
-                    : <WorkshopUpcomingPlaceholder />
+                            </Grid>
+                        </Box>
+                        : <WorkshopUpcomingPlaceholder />
+                    :
+
+                    sampleData.length > 0 ?
+                        <Box sx={{ flexGrow: 1, width: '90%' }}>
+                            <Grid container spacing={4}>
+                                {sampleData.map((data) => (
+                                    <Grid item xs={5}>
+                                        {/* placeholder */}
+                                        <AdminWorkshopCard workshop={data}></AdminWorkshopCard>
+                                    </Grid>
+                                ))
+                                }
+
+
+                            </Grid>
+                        </Box>
+                        : <WorkshopAssignmentPlaceholder />
                 }
+
+
             </div>
         </div >
     );
