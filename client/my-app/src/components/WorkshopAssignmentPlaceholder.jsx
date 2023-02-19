20bbbafa91e8c3eee4312f32aea52053afe378e2
Natalie Abreu <nrabreu@usc.edu>
2023-02-18 16:58:34 -0800

    Updates assignment page

--- client/my-app/src/components/WorkshopAssignmentPlaceholder.jsx
+++ client/my-app/src/components/WorkshopAssignmentPlaceholder.jsx
@@ -11,32 +11,34 @@ const WorkshopAssignmentPlaceholder = () => {
         >
             <div
                 style={{
-                    position: "absolute",
-                    left: "28.3%",
-                    top: "47.1%",
+                    display: 'flex',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                    margin: 'auto',
+                    flexDirection: 'column',
                     height: "325px",
                 }}
             >
                 <img src={emptyWorkshopAssignment}
-                style={{
-                    width: "auto",
-                    height: "233px",
-                    display: "flex",
-                    margin: "auto",
-                }} 
+                    style={{
+                        width: "auto",
+                        height: "233px",
+                        display: "flex",
+                        margin: "auto",
+                    }}
                 />
                 <p
-                style={{
-                    paddingTop: "44px",
-                    fontFamily: "Roboto Mono",
-                    fontWeight: "700",
-                    fontSize: "20px",
-                    lineHeight: "28px",
-                    textAlign: "center",
-                    color: "#2F2F2F",
-                    display: "flex",
-                    margin: "auto",
-                }}
+                    style={{
+                        paddingTop: "44px",
+                        fontFamily: "Roboto Mono",
+                        fontWeight: "700",
+                        fontSize: "20px",
+                        lineHeight: "28px",
+                        textAlign: "center",
+                        color: "#2F2F2F",
+                        display: "flex",
+                        margin: "auto",
+                    }}
                 >
                     Nothing to see here!  <br />
                     Stay tuned for more assignments
