7f4caafb524811181d9751f4b36f2d6e8682f492
Raymond Kuan <rkuan@usc.edu>
2023-03-06 00:27:00 -0800

    fixed logout functionality

--- client/my-app/src/components/Header.jsx
+++ client/my-app/src/components/Header.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import headerLogo from '../images/logo.svg';
 import emailLogo from '../images/email.svg';
 import logoutImage from '../images/logout.svg';
+import { useHistory } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
 
 
 const Header = () => {
-    return (
 
+    const history = useHistory();
+    const [cookies, setCookie] = useCookies(['user']);
+
+    function logout() {
+        setCookie('user_id', -1, { path: '/' , secure: 'true'});
+        history.push("/");
+        document.location.reload()
+    }
+
+    return (
         <div style={{ fontSize: "50px", height: "130px" }}>
             <div className="border d-flex align-items-center" style={{ position: "relative", height: "100%", width: "full", boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
                 <img src={headerLogo} alt="Boundless Brilliance logo" style={{ position: "relative", left: "6.0%", width: "21.52%", bottom: '6px' }} />
@@ -17,8 +29,7 @@ const Header = () => {
                     {/* <img src={emailLogo} alt="Email logo" style={{ position: "absolute", right: "6.5%", width: "1.785%" }} /> */}
                 </a>
                 
-                <img src={logoutImage} alt="Logout Image" style={{ position: "absolute", right: "2.5%", width: "1.5%" }}></img>
-                
+                <img src={logoutImage} alt="Button Image" onClick={logout} style={{ position: "absolute", right: "2.5%", width: "1.9%", top: "50%", transform: "translateY(-50%)" }} />
 
             </div>
         </div>
