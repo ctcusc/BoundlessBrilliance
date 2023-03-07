b420c6f9f0187f0d123d3814a0a7feee7ebf731c
Raymond Kuan <rkuan@usc.edu>
2023-03-06 17:50:20 -0800

    database debug

--- client/my-app/src/components/Header.jsx
+++ client/my-app/src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
             <div className="border d-flex align-items-center" style={{ position: "relative", height: "100%", width: "full", boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)' }}>
                 <img src={headerLogo} alt="Boundless Brilliance logo" style={{ position: "relative", left: "6.0%", width: "21.52%", bottom: '6px' }} />
                 {/* <a href="mailto:recipient@example.com?subject=Subject%20line&body=Body%20text"> */}
-                <a href="https://mail.google.com/mail/?view=cm&to=cherisecharleswell@boundlessbrilliance.org">
+                <a href="https://mail.google.com/mail/?view=cm&to=info@boundlessbrilliance.org">
                     <img src={emailLogo} alt="Email logo" style={{ position: "absolute", right: "6.5%", width: "1.7%", top: "50%", transform: "translateY(-50%)" }} />
 
                     {/* <img src={emailLogo} alt="Email logo" style={{ position: "absolute", right: "6.5%", width: "1.785%" }} /> */}
