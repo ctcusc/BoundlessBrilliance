413971ebaf5adf6f54be3389cc104618764e19a5
Raymond Kuan <rkuan@usc.edu>
2023-04-05 19:41:37 -0700

    added logout function

--- client/my-app/src/components/WorkshopHeader.jsx
+++ client/my-app/src/components/WorkshopHeader.jsx
@@ -9,6 +9,15 @@ import { useCookies } from "react-cookie";
 
 
 const WorkshopHeader = () => {
+    const history = useHistory();
+    const [cookies, setCookie] = useCookies(['user']);
+
+    function logout() {
+        setCookie('user_id', -1, { path: '/' , secure: 'true'});
+        history.push("/");
+        document.location.reload()
+    }
+
     return (
         <div style={{ fontSize: "50px" }}>
             <div style={{ position: "relative", height: "100%", width: "full"}}>
@@ -16,7 +25,7 @@ const WorkshopHeader = () => {
                     <img src={emailLogo} alt="Email logo" style={{ position: "absolute", right: "6.5%", width: "2%", top: "50%", transform: "translateY(-50%)" }} />
                 </a>
                 <a>
-                    <AccountCircleIcon style={{ position: "absolute", right: "2.5%", width: "3%", top: "50%", transform: "translateY(-50%)" }}/>
+                    <img src={logoutImage} alt="Button Image" onClick={logout} style={{ position: "absolute", right: "2.5%", width: "2.3%", top: "50%", transform: "translateY(-50%)" }} />
                 </a>
             </div>
         </div>
