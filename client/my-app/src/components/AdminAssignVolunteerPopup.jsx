6b49185018fa847947e1a90e56b2a11ba627c9d2
Raymond Kuan <rkuan@usc.edu>
2023-04-09 20:15:09 -0700

    Added backend functionality to assign user to workshop

--- client/my-app/src/components/AdminAssignVolunteerPopup.jsx
+++ client/my-app/src/components/AdminAssignVolunteerPopup.jsx
@@ -2,12 +2,8 @@ import './WorkshopPopup.css';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import { Autocomplete, InputAdornment, TextField, Paper, Chip } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import volunteer_list from './volunteers.json'
-import headerLogo from '../images/login_logo.svg';
 import ClearIcon from '@mui/icons-material/Clear';
-import moment from 'moment';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react"
 
 
 const StyledTextField = styled(TextField) ({
@@ -80,21 +76,78 @@ const VListComponent = ({ myList }) => {
   };
 
 const AdminAssignVolunteerPopup = ({props, setToggleState}) => {
+    useEffect(() => {
+        fetch(`/api/allActiveUserNames`)
+            .then(response => response.json())
+            .then(data => setVolunteers(data))
+            .catch(error => console.error(error));
+
+        fetch(`/api/allActiveUserIDName`)
+            .then(response => response.json())
+            .then(data => setSelectedVolunteersId(data))
+            .catch(error => console.error(error));
+        
+        const workshopId = props.workshop.id; 
+        fetch(`/api/userAssignedWorkshop?workshop_id=${workshopId}`)
+            .then(response => response.json())
+            .then(data => setSelectedVolunteers(data))
+            .catch(error => console.error(error));
+
+        fetch(`/api/userAssignedWorkshop?workshop_id=${workshopId}`)
+            .then(response => response.json())
+            .then(data => setOriginalVolunteers(data))
+            .catch(error => console.error(error));
+        
+        fetch(`/api/userAssignedWorkshopStatusA?workshop_id=${workshopId}`)
+            .then(response => response.json())
+            .then(data => setOriginalAccepted(data))
+            .catch(error => console.error(error));
+        
+        fetch(`/api/userAssignedWorkshopStatusB?workshop_id=${workshopId}`)
+            .then(response => response.json())
+            .then(data => setOriginalUndecided(data))
+            .catch(error => console.error(error));
+
+        fetch(`/api/userAssignedWorkshopStatusC?workshop_id=${workshopId}`)
+            .then(response => response.json())
+            .then(data => setOriginalDeclined(data))
+            .catch(error => console.error(error));
+        
+    }, []);
+
     const [SearchValues, setSearchValues] = useState(false);
-    const [volunteers, setVolunteers] = useState([]);
-    const [selectedVolunteers, setSelectedVolunteers] = useState([]);
+
+    // The following vars are used for backend
+    const [volunteers, setVolunteers] = useState([]); //all avaliable volunteers
+    const [selectedVolunteers, setSelectedVolunteers] = useState([]); //volunteers that are already assigned
+    const [selectedVolunteersId, setSelectedVolunteersId] = useState([]); //ID of volunteers
+    const [OriginalVolunteers, setOriginalVolunteers] = useState([]); //original list used for comparisons
+
+    // The following vars are used for frontend display
+    const [OriginalAccepted, setOriginalAccepted] = useState([]); //original list -> accepted
+    const [OriginalUndecided, setOriginalUndecided] = useState([]); //original list -> undecided
+    const [OriginalDeclined, setOriginalDeclined] = useState([]); //original list -> declined
+    const [NewlySelected, setNewlySelected] = useState([]); //original list -> declined
 
     const handleVolunteerSelection = (event, value) => {
         if (value  && !selectedVolunteers.includes(value)) {
         setSelectedVolunteers([...selectedVolunteers, value]);
         }
+
+        if (value  && !NewlySelected.includes(value)) {
+            setNewlySelected([...NewlySelected, value]);
+        }
     };
 
     const handleDelete = (volunteerToDelete) => () => {
         setSelectedVolunteers(selectedVolunteers.filter((volunteer) => volunteer !== volunteerToDelete));
+        setOriginalAccepted(OriginalAccepted.filter((volunteer) => volunteer !== volunteerToDelete));
+        setOriginalUndecided(OriginalUndecided.filter((volunteer) => volunteer !== volunteerToDelete));
+        setOriginalDeclined(OriginalDeclined.filter((volunteer) => volunteer !== volunteerToDelete));
+        setNewlySelected(NewlySelected.filter((volunteer) => volunteer !== volunteerToDelete));
     };
 
-    const filteredOptions = volunteer_list.filter((volunteer) => !selectedVolunteers.includes(volunteer));
+    const filteredOptions = volunteers.filter((volunteer) => !selectedVolunteers.includes(volunteer));
 
     function setToggle() {
         setToggleState(false);
@@ -103,6 +156,59 @@ const AdminAssignVolunteerPopup = ({props, setToggleState}) => {
     const handleSubmit = (event) => {
         event.preventDefault();
 
+        async function addAssignment(user_id, workshop_id) {
+            const response = await fetch('/api/assignUser', {  
+                method: 'post',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(
+                    { 
+                        'user_id': user_id, 
+                        'workshop_id': workshop_id, 
+                    }),
+            })
+            const data = await response.json();
+        }
+
+        async function removeAssignment(user_id, workshop_id) {
+            const response = await fetch(`/api/removeAssignment?user_id=${user_id}&workshop_id=${workshop_id}`, {  
+                method: 'delete',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ 
+                  'user_id': user_id, 
+                  'workshop_id': workshop_id, 
+                }),
+              });
+            const data = await response.json();
+        }
+
+        function getUserIdByName(name) {
+            const user = selectedVolunteersId.find(user => user.name === name);
+            return user ? user.user_id : null;
+        }
+
+        function isNameInArray(name, array) {
+            return array.includes(name);
+          }
+
+        var workshop_id = props.workshop.id;
+        for (let i = 0;i < selectedVolunteers.length;i ++) {
+            //Make API Call
+            if(isNameInArray(selectedVolunteers[i], OriginalVolunteers) == false){
+                var user_id = (getUserIdByName(selectedVolunteers[i]));
+                addAssignment(user_id, workshop_id);
+            }
+        }
+
+        for (let i = 0;i < OriginalVolunteers.length;i ++) {
+            //Make API Call
+            if(isNameInArray(OriginalVolunteers[i], selectedVolunteers) == false){
+                user_id = (getUserIdByName(OriginalVolunteers[i]));
+                removeAssignment(user_id, workshop_id);
+            }
+        }
+
+        document.location.reload();
+
     };
 
     return (
@@ -110,7 +216,7 @@ const AdminAssignVolunteerPopup = ({props, setToggleState}) => {
             <div className="volunteer-container">
                 <div style={{ marginBottom: '20%' }}>
                     <div style={{fontSize: '16px', fontFamily: 'Avenir', position: 'absolute', top: '30px', left: '30px'}}>Assign Volunteer</div>
-                    <div style={{fontSize: '24px', fontFamily: 'Avenir', fontWeight: 'bold', position: 'absolute', top: '60px', left: '30px'}}>Workshop Title</div>
+                    <div style={{fontSize: '24px', fontFamily: 'Avenir', fontWeight: 'bold', position: 'absolute', top: '60px', left: '30px'}}> {props.workshop.name}</div>
                     <button onClick={setToggle} className="popup-admin-icon-corner">
                         <StyledClearIcon />
                     </button>
@@ -123,9 +229,39 @@ const AdminAssignVolunteerPopup = ({props, setToggleState}) => {
                     gap: '4px', 
                     margin: '4px 0',  
                 }}>
-                    {selectedVolunteers.map((volunteer) => (
+                    {OriginalAccepted.map((volunteer) => (
                     <Chip 
-                        style={{ borderColor: "#1398A0", color: "#1398A0", mr: 1,}} 
+                        style={{ borderColor: "#1398A0", color: "#0e8830",  backgroundColor:'#FFFFFF', mr: 1,}} 
+                        key={volunteer} 
+                        variant="outlined" 
+                        label={volunteer} 
+                        onDelete={handleDelete(volunteer)}
+                        deleteIcon={<ClearIcon style={{ color: '#1398A0' }} />}
+                    />
+                    ))}
+                    {OriginalDeclined.map((volunteer) => (
+                    <Chip 
+                        style={{ borderColor: "#1398A0", color: "#d90000",  backgroundColor:'#FFFFFF',  mr: 1,}} 
+                        key={volunteer} 
+                        variant="outlined" 
+                        label={volunteer} 
+                        onDelete={handleDelete(volunteer)}
+                        deleteIcon={<ClearIcon style={{ color: '#1398A0' }} />}
+                    />
+                    ))}
+                     {OriginalUndecided.map((volunteer) => (
+                    <Chip 
+                        style={{ borderColor: "#1398A0", color: "#1398A0", backgroundColor:'#FFFFFF',  mr: 1,}} 
+                        key={volunteer} 
+                        variant="outlined" 
+                        label={volunteer} 
+                        onDelete={handleDelete(volunteer)}
+                        deleteIcon={<ClearIcon style={{ color: '#1398A0' }} />}
+                    />
+                    ))}
+                     {NewlySelected.map((volunteer) => (
+                    <Chip 
+                        style={{ borderColor: "#1398A0", color: "#1398A0", backgroundColor:'#FFFFFF',  mr: 1,}} 
                         key={volunteer} 
                         variant="outlined" 
                         label={volunteer} 
@@ -155,7 +291,7 @@ const AdminAssignVolunteerPopup = ({props, setToggleState}) => {
                     </div>
 
                     <div className="popup-assign-button">
-                        <ContainedButton onClick={handleSubmit}>Assign</ContainedButton>
+                        <ContainedButton onClick={handleSubmit}>Update</ContainedButton>
                     </div>
                 </div>
             </div>
