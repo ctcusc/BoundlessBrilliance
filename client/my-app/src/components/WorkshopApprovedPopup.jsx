2c461a02c412499fa1da9fc17b21efa449ed6d0d
oliviasl <oslai@usc.edu>
2023-04-12 18:23:35 -0700

    Workshop user popups - delete contact admin link

--- client/my-app/src/components/WorkshopApprovedPopup.jsx
+++ client/my-app/src/components/WorkshopApprovedPopup.jsx
@@ -39,7 +39,7 @@ const WorkshopApprovedPopup = ({setToggleState}) => {
                     <StyledCheckCircleIcon />
                 </div>
                 <h1 className="h1-popup">Workshop Approved</h1>
-                <p className="p-popup">To undo this action, you must <span><a className="a-popup" href="#">contact admin</a></span></p>
+                <p className="p-popup">To undo this action, you must contact admin.</p>
             </div>
             
         </div>
