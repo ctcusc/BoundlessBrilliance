a737c93eebffa1a08afd02eb7c7ae011b225e326
oliviasl <oslai@usc.edu>
2023-04-12 18:15:33 -0700

    Fix more formatting in user.controller.js

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -35,57 +35,57 @@ class userController {
 
   async validateUser(req) {
      try{
-         const idQuery = await db.query(
-             "select user_id from master_users WHERE user_email = $1",
-             [req.body.user_email]
-         );
+      const idQuery = await db.query(
+          "select user_id from master_users WHERE user_email = $1",
+          [req.body.user_email]
+      );
 
-         if (idQuery.rows.length == 0) { //username doesn't exist
-             return [-1, -1, -1];
-         }
-         else {
-            const user_id = idQuery.rows[0].user_id;
-            
-            const passwordQuery = await db.query( //get password
-              "select user_password from master_users WHERE user_id = $1",
+      if (idQuery.rows.length == 0) { //username doesn't exist
+          return [-1, -1, -1];
+      }
+      else {
+        const user_id = idQuery.rows[0].user_id;
+        
+        const passwordQuery = await db.query( //get password
+          "select user_password from master_users WHERE user_id = $1",
+          [user_id]
+        );
+
+        const stored_password = passwordQuery.rows[0].user_password;
+        const entered_password = req.body.user_password;
+
+        if (stored_password != entered_password){ //compare password
+          return [-1, -1, -1];
+        }
+
+        const statusQuery = await db.query( //get user status
+          "select user_status from user_status WHERE user_id = $1",
+          [user_id]
+        );
+
+        const userStatus = statusQuery.rows[0].user_status;  // 0 = Not Approved, 1 = Approved
+
+        const isAdminQuery = await db.query( //get user status
+              "select is_admin from master_users WHERE user_id = $1",
               [user_id]
-            );
+        );
 
-            const stored_password = passwordQuery.rows[0].user_password;
-            const entered_password = req.body.user_password;
+        const isAdmin = isAdminQuery.rows[0].is_admin;
 
-            if (stored_password != entered_password){ //compare password
-              return [-1, -1, -1];
+        if (userStatus == 0){
+            return [0, 0, -1]; // user not approved
+        }
+        else {
+            if (isAdmin == 1){
+              return [1, 1, user_id];
+            } else {
+              return [1, 0, user_id]; 
             }
- 
-             const statusQuery = await db.query( //get user status
-                 "select user_status from user_status WHERE user_id = $1",
-                 [user_id]
-             );
- 
-            const userStatus = statusQuery.rows[0].user_status;  // 0 = Not Approved, 1 = Approved
-
-            const isAdminQuery = await db.query( //get user status
-                 "select is_admin from master_users WHERE user_id = $1",
-                 [user_id]
-             );
-
-            const isAdmin = isAdminQuery.rows[0].is_admin;
-
-            if (userStatus == 0){
-                return [0, 0, -1]; // user not approved
-            }
-            else{
-                if (isAdmin == 1){
-                  return [1, 1, user_id];
-                } else {
-                  return [1, 0, user_id]; 
-                }
-            }
-         } 
-         }catch (error){
-             return [-1, -1, -1]; // other error cases
-         }
+        }
+      } 
+    } catch (error){
+      return [-1, -1, -1]; // other error cases
+    }
   }
 
   async approveUser(user_id) {
@@ -313,9 +313,9 @@ class userController {
         total: total
       };
       return res;
-      } catch(error){
-        return error;
-      }
+    } catch(error){
+      return error;
+    }
   }
 
   async generateGenderMetrics() {
@@ -324,9 +324,9 @@ class userController {
         "SELECT user_gender as name, CAST(COUNT(*) as int) as value FROM master_users GROUP BY user_gender;",
       );
       return data.rows;
-      } catch(error){
-        return error;
-      }
+    } catch(error){
+      return error;
+    }
   }
 
   async generateEthnicityMetrics() {
@@ -335,9 +335,9 @@ class userController {
         "SELECT user_ethnicity as name, CAST(COUNT(*) as int) as value FROM master_users GROUP BY user_ethnicity;",
       );
       return data.rows;
-      } catch(error){
-        return error;
-      }
+    } catch(error){
+      return error;
+    }
   }
 
   async approveAllUsers() {
@@ -346,9 +346,9 @@ class userController {
         "UPDATE user_status SET user_status = 1 WHERE user_status = 0;",
       );
       return 0;
-      } catch(error){
-        return error;
-      }
+    } catch(error){
+      return error;
+    }
   }
 
 } 
