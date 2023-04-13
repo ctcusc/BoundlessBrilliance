ef94d956a0f1d4a04c83bf97582da2f54390244b
Raymond Kuan <rkuan@usc.edu>
2023-04-13 12:14:33 -0700

    deploy: update index.js

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
 const { Pool } = require("pg");
+const pg = require('pg');
 
 const pool = new Pool();
+
+// const pool = new pg.Pool({
+//   connectionString: process.env.POSTGRESQL_EXTERNAL_URL,
+//   database: 'bb_database',
+// });
+
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
